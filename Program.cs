1d8702ba802b29fe4ae529fa1c67bc91910a4896
SajeevDarshanapu <67383521+SajeevDarshanapu@users.noreply.github.com>
2022-02-27 14:44:25 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+namespace Hey
+{
+    public class Hello
+    {
+        public static void Main(string[]args)
+        {
+            int f = 1, i, num;
+            Console.WriteLine("Enter a number");
+            num = Convert.ToInt32(Console.ReadLine());
+            for(i = 1; i <= num; i++)
+            {
+                f = f * i;
+            }
+            Console.WriteLine(f);
+
+        }
+    }
+}
